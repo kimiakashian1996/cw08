3f2e8ba05cec4684c498350653553962f1781d79
hadisAhmadi <hadiseh.ahmadi.aghdam@gmail.com>
2024-01-19 14:29:39 +0330

    add drugs to sql

--- src/main/java/table.sql
+++ src/main/java/table.sql
@@ -37,3 +37,14 @@ CREATE TABLE IF NOT EXISTS drug_list
 
     );
 
+insert into drug values (DEFAULT,'ADRENALINE',10,2);
+insert into drug values (DEFAULT,'NORADRENALINE',20,5);
+insert into drug values (DEFAULT,'CALCIUM',10,6);
+insert into drug values (DEFAULT,'DOPAMINE',40,2);
+insert into drug values (DEFAULT,'ATROPINE',70,2);
+insert into drug values (DEFAULT,'AMIODARONE',30,9);
+insert into drug values (DEFAULT,'ADENOSINE',80,1);
+insert into drug values (DEFAULT,'LIDOCAINE',40,7);
+
+
+select * from drug;
