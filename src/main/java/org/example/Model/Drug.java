a407c75d45bb88a333506491ab569f3104a433c8
hadisAhmadi <hadiseh.ahmadi.aghdam@gmail.com>
2024-01-19 14:20:14 +0330

    create enum drug

--- src/main/java/org/example/Model/Drug.java
+++ src/main/java/org/example/Model/Drug.java
@@ -1,66 +1,12 @@
 package org.example.Model;
 
-public class Drug {
-    private int id ;
-    private String drugName ;
-    private int price;
-    private int inventory;
-
-    public Drug() {
-    }
-
-    public Drug(int id, String drugName, int price, int inventory) {
-        this.id = id;
-        this.drugName = drugName;
-        this.price = price;
-        this.inventory = inventory;
-    }
-
-    public Drug(String drugName, int price, int inventory) {
-        this.drugName = drugName;
-        this.price = price;
-        this.inventory = inventory;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getDrugName() {
-        return drugName;
-    }
-
-    public void setDrugName(String drugName) {
-        this.drugName = drugName;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(int inventory) {
-        this.inventory = inventory;
-    }
-
-    @Override
-    public String toString() {
-        return "DrugService{" +
-                "id=" + id +
-                ", drugName='" + drugName + '\'' +
-                ", price=" + price +
-                ", inventory=" + inventory +
-                '}';
-    }
+public enum Drug {
+    ADRENALINE,
+    NORADRENALINE,
+    DOPAMINE,
+    CALCIUM,
+    ATROPINE,
+    AMIODARONE,
+    LIDOCAINE,
+    ADENOSINE
 }
